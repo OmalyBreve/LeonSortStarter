4d800fb1b0b6c2965136ca49386f126bd5bec3dc
Omaly Breve <omalybreve@yahoo.es>
2020-03-19 20:24:22 -0600

    Trabajando en el Main

--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -1,21 +1,49 @@
 package edu.ujcv.progra1;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        int[] elementos = {3,2,4,5,6,7,9,9,10,3,5};
-        QuickSort s = new QuickSort();
-        System.out.println(s.quick(elementos,0,elementos.length ));
+        int[] array = new int[100];
 
-        System.out.println(elementos);
-
-        for (int i = 0; i < elementos.length; i++) {
-            System.out.println(elementos[i]);
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.abs(random.nextInt(10000000));
         }
 
-        //elementos = new int[]{3,2,4,5,6,7,9,9,10,3,5};
-        //System.out.println("\n\ntarda "+ s.sort(elementos)+ " milisegundos");
+        BubleSort b =new BubleSort();
+        array = b.bubleSort(array);
+
+        ShuffleSort s = new ShuffleSort();
+        array = s.shuffleSort(array);
+
+        MergeSort m = new MergeSort();
+        array = m.mergeSort(array);
+
+        QuickSort q = new QuickSort();
+        array = q.quicksort1(array);
+
+        HeapSort h = new HeapSort();
+        h.heapSort(array);
+
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]+", ");
+        }
+
+
+        System.out.println("\n\nBubleSort tarda "+ b.sort(array)+ " milisegundos");
+        //System.out.println("\n\nShuffleSort tarda "+ s.sort(array)+ " milisegundos");
+        System.out.println("\n\nMergeSort tarda "+ m.sort(array)+ " milisegundos");
+        System.out.println("\n\nQuickSort tarda "+ q.sort(array)+ " milisegundos");
+        System.out.println("\n\nHeapSort tarda "+ h.sort(array)+ " milisegundos");
+
+
+
     }
+
+
+
 }
--- src/edu/ujcv/progra1/MergeSort.java
+++ src/edu/ujcv/progra1/MergeSort.java
@@ -1,22 +1,74 @@
-package edu.ujcv.progra1;
+ package edu.ujcv.progra1;
 
-public class MergeSort  implements SortTester{
+public class MergeSort implements SortTester{
+
     @Override
     public long sort(int[] array) {
-        long start = System.currentTimeMillis();
+        long start = System.nanoTime();
         mergeSort(array);
 
-        long end = System.currentTimeMillis();
+        long end = System.nanoTime();
 
         return end - start;
     }
 
-    public static int[] mergeSort(int[] a){
-        return new int[5];
+    public int[] mergeSort(int[] a) {
+
+        int[] inf = new int [a.length/2];
+        int[] sup = new int [a.length - inf.length];
+        int k =0;
+
+        if(a.length <= 1){
+            return a;
+        }
+
+        for (int i = 0; i < inf.length; i++) {
+
+            inf [i] = a [k++];
+
+        }
+
+        for (int i = 0; i < sup.length; i++) {
+
+            sup[i]= a[k++];
+        }
+
+
+        return merge(mergeSort(inf),mergeSort(sup));
     }
 
     public static int[] merge(int[] inf, int[] sup ){
-        return new int[5];
+
+        int [] retval = new int [inf.length + sup.length];
+
+        int i = 0, j = 0, k = 0;
+
+        for (  ; j < inf.length && k < sup.length; i++) {
+
+            if (inf[j] < sup[k]) {
+
+                retval[i] = inf[j++];
+
+            } else {
+
+                retval[i] = sup[k++];
+            }
+
+        }
+
+        if (j < inf.length){
+            for ( ; i < retval.length; i++) {
+                retval[i] = inf[j++];
+            }
+        }else if (k < sup.length){
+            for (; i < retval.length ; i++) {
+                retval[i] = sup[k++];
+
+            }
+
+        }
+
+        return retval;
     }
 
 
